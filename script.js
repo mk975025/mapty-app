a7adefe2e93273170ecddae68452d05e215c46c5
Matthew <matthewkimcs@gmail.com>
2022-12-25 22:55:41 -0800

    finishing changes on zoom out restrictions, need to do more research on leaflet docs tomorrow

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function success(pos) {
   L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-    maxBoundsViscosity: 1.0,
     noWrap: true,
   }).addTo(map);
 
