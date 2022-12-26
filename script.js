e639ce9680b7c047bedc4dcf5ea99da284185ed7
Matthew <matthewkimcs@gmail.com>
2022-12-25 21:44:14 -0800

    retrieved user coordinates via geolocation API and displayed google maps on user location

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+//gather and set user coordinates via geolocation api with google maps
+function success(pos) {
+  const { latitude, longitude } = pos.coords;
+  const map = L.map('map').setView([latitude, longitude], 13);
+  L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+  }).addTo(map);
+
+  L.marker([latitude, longitude])
+    .addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+}
+
+function error() {
+  alert('Could not get your position');
+}
+
+navigator.geolocation?.getCurrentPosition(success, error);
